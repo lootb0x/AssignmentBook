cc53ae735f9c04a136f41411ab9741bc9610366f
lootb0x <justinlin8031@gmail.com>
2025-02-06 14:31:09 +0000

    asdraf

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 public Main class{
 public static void main(String[] args){
-
-
+    Boolean schedule = new Boolean[8][60];
+    AppointmentBook A = new AppointmentBook(schedule);
+    for(int i =10; )
 }
 }
